3195bb9e92cc544ec1e0a7ae848791f0bd33b198
BoyChai <1972567225@qq.com>
2023-05-31 15:34:59 +0800

    Add Read And Verification Configuration

---
+++ config/read.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"github.com/BoyChai/Ant"
+	"github.com/spf13/viper"
+	"os"
+)
+
+func ReadConfig() error {
+	viper.SetConfigType("yaml")
+
+	file, err := os.Open("./Honeycomb.yaml")
+	if err != nil {
+		return err
+	}
+	err = viper.ReadConfig(file)
+	if err != nil {
+		return err
+	}
+	err = viper.Unmarshal(&Cfg)
+	if err != nil {
+		return err
+	}
+	validator := Ant.New()
+	e := validator.Struct(Cfg)
+	if e.Is {
+		return errors.New(fmt.Sprint(e.Data))
+	}
+	return nil
+}
--- config/type.go
+++ config/type.go
@@ -12,7 +12,7 @@ type config struct {
 		Listen AddrName `yaml:"listen"`
 		Nodes  []struct {
 			Name     string   `yaml:"name"`
-			Protocol Protocol `yaml:"protocol"`
+			Protocol Protocol `yaml:"protocol" ant:"network_transport_protocol_common"`
 			Addr     AddrName `yaml:"addr"`
 		} `yaml:"nodes"`
 	} `yaml:"penetrate"`
@@ -21,20 +21,20 @@ type config struct {
 		Server  AddrName `yaml:"server"`
 		Service []struct {
 			Name     string   `yaml:"name"`
-			Protocol Protocol `yaml:"protocol"`
+			Protocol Protocol `yaml:"protocol" ant:"network_transport_protocol_common"`
 			Addr     AddrName `yaml:"addr"`
 		} `yaml:"service"`
 	} `yaml:"client"`
 	// 流量转发配置
 	Forward []struct {
 		Name     string   `yaml:"name"`
-		Protocol Protocol `yaml:"protocol"`
+		Protocol Protocol `yaml:"protocol" ant:"network_transport_protocol_common"`
 		Listen   AddrName `yaml:"listen"`
 		Target   AddrName `yaml:"target"`
 	} `yaml:"forward"`
 	Load []struct {
 		Name     string     `yaml:"name"`
-		Protocol Protocol   `yaml:"protocol"`
+		Protocol Protocol   `yaml:"protocol" ant:"network_transport_protocol_common"`
 		Servers  []AddrName `yaml:"servers"`
 	} `yaml:"load"`
 	Address []Addr
@@ -44,11 +44,11 @@ type AddrName string
 
 type Protocol string
 
-var TCP Protocol = "TCP"
-var UDP Protocol = "UDP"
+const TCP Protocol = "TCP"
+const UDP Protocol = "UDP"
 
 type Addr struct {
 	Name string `yaml:"name"`
 	Addr string `yaml:"addr"`
-	Port int    `yaml:"port"`
+	Port int    `yaml:"port" ant:"port"`
 }
